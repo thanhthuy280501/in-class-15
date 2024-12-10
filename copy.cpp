bee35d518623c053b06981118d18aabe69c2c093
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-10 13:07:26 -0500

    Update copy.cpp

--- copy.cpp
+++ copy.cpp
@@ -14,51 +14,57 @@ using namespace std;
 // This function returns the average score in the file that is
 // passed to it as an argument.
 //*********************************************************************
-int averageScore(char name[]) {
-
+class FileError{
+public:
+  FileError(){}
+  FileError ( string theString) : fileName (theString){}
+  string getName() { return fileName; }
+private:
+  string fileName;
+};
+//*************************************************************************
+// This function returns the average score in the file that is
+// passed to it as an argument
+//*************************************************************************
+int averageScore (char name[]){
   ifstream f;
   int count = 0;
   int num;
   int sum = 0;
-
-  f.open(name);
-  if ( f.fail())
-    throw FileError(name);
-  
-  
-  //*************************************************************
-  // Scan through the file to add scores to a sum.
-  //*************************************************************
-  while (f >> num) {
-    sum = sum + num;
-    count++;
-  }
-  f.close();
-  // cosider;
-  // 
-  if (cout ==0)
-  throw (count);
-
-  return (sum / count);
+f.open(name);
+if (f.fail())
+  throw FileError(name);
+//*************************************************************************
+// Scan through the file to add scores to a sum
+//*************************************************************************
+While ( f> num){
+sum = sum + num;
+count++;
 }
-
-int main(int argc, char *argv[]) {
+f.close();
+return (sum/ count);
+}
+int main ( int argc, char *argv []){
   int avg;
-  if (argc ==1)
-    throw argc;
-  if (argc >2 ){
-    string s = argv[2];
+  char filename[20];
+  try {
+    if ( argc == 1)
+      throw argc;
+    if ( argc > 2 ){
+    string s = argv [2];
     throw s;
+    }
   }
-}
-try {
+catch ( int e) {
+  char filename[20] = "scores.txt";
 
-  int avg = averageScore(argv[1]);
+  cout << "You didn't specify a file name, so I'm confusing scores.txt by default" << endl;
+agv = averageScore (filename);
 }
-  Catch (FileError e){
-  cout << "That file " << e.getName() << "had a problem" 
+catch (string x){
+cout << "You have tpp many file name arguments " << endl;
+cout << " I am ignoring this argument: " << x << endl;
 
-  cout << avg << endl;
-  
-  return 0;
-}
+avg= averageScore(argv[1]);
+
+
